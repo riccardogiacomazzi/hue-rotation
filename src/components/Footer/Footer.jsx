4dda3bcd7c626d3b79540dbf6a9d5c882ce226e8
riccardogiacomazzi <riccardogiacomazzi93@gmail.com>
2025-05-21 20:50:39 +0200

    Improved landing page styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./Footer.css";
-import Slider from "./Slider/Slider";
+// import Slider from "./Slider/Slider";
+import * as Slider from "@radix-ui/react-slider";
 
 const Footer = ({ gradientParameters, onSubmit, loading }) => {
   const [sliderValues, setSliderValues] = useState([0, 0, 0]);
@@ -33,22 +34,40 @@ const Footer = ({ gradientParameters, onSubmit, loading }) => {
       <div className="sliders-container">
         {gradientParameters &&
           gradientParameters.map((item, index) => (
-            <Slider
-              onChange={(value) => handleSliderChange(index, value)}
-              className="footer-item"
-              key={index}
-              label={item}
-              value={sliderValues[index]}
-            >
-              {item}
-            </Slider>
+            <div className="footer-item">
+              <div className="slider-label">{item}</div>
+              <Slider.Root
+                className="slider-root"
+                defaultValue={[0]}
+                max={100}
+                step={1}
+                onValueChange={(value) => handleSliderChange(index, value)}
+                key={index}
+              >
+                <Slider.Track className="slider-track">
+                  <div className="slider-bar" style={{ width: `${sliderValues[index]}%` }} />
+                  <Slider.Range className="slider-range" />
+                </Slider.Track>
+                <Slider.Thumb className="slider-thumb" aria-label="Slider Thumb" />
+              </Slider.Root>
+            </div>
+
+            // <Slider
+            //   onChange={(value) => handleSliderChange(index, value)}
+            //   className="footer-item"
+            //   key={index}
+            //   label={item}
+            //   value={sliderValues[index]}
+            // >
+            //   {item}
+            // </Slider>
           ))}
         <div
           className="footer-item"
-          style={{ backgroundColor: clickFlashed ? "black" : "white", mixBlendMode: loading && "difference" }}
+          style={{ backgroundColor: clickFlashed ? "white" : "black", mixBlendMode: loading && "difference" }}
           onClick={(e) => handleSubmit(e, sliderValues)}
         >
-          {clickFlashed ? "Generating" : "Generate"}
+          <div className="button-text">{clickFlashed ? "Generating" : "Generate"}</div>
         </div>
       </div>
     </div>
